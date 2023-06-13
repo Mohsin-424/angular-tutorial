4f8cbbbb43ade2eb4086d442510315cf79381751
Mohsinpu424 <mi873345@gmail.com>
2023-06-13 17:31:34 +0500

    mian

--- src/app/even/even.component.ts
+++ src/app/even/even.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class EvenComponent implements OnInit {
   @Input() number!: number;
 
-  constructor() {
+  constructor()
+  {
 
   }
 
