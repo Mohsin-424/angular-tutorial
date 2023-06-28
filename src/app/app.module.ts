1f7c2431689677f06ef9e6578fd10b80d33727f3
Mohsinpu424 <mi873345@gmail.com>
2023-06-28 11:55:00 +0500

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Task1Component } from './task1/task1.component';
+import { BetterHighlightDirective } from './better-highlight.directive';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { Task1Component } from './task1/task1.component';
     AppComponent,
     Task1Component,
     BasicHighlightDirective,
+    BetterHighlightDirective,
   ],
 
   imports: [
