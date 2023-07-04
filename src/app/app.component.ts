c116d20bf4eb78558c8add0018f66f1edc8c5531
Mohsinpu424 <mi873345@gmail.com>
2023-07-04 11:41:06 +0500

    maij

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'service';
+  accounts = [
+    {
+      name: 'Master Account1',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount2',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account3',
+      status: 'unknown'
+    }
+  ];
+
+  onAccountAdded(newAccount: {name: string, status: string}) {
+    this.accounts.push(newAccount);
+  }
+
+  onStatusChanged(updateInfo: 
+    {id: number, newStatus: string}) {
+    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  }
 }
