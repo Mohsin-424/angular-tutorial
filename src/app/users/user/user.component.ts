a95623b41b0d2b32cfcfa4d97b99aa1c9fc80520
Mohsinpu424 <mi873345@gmail.com>
2023-07-05 16:41:51 +0500

    main

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   user!: {id: number, name: string};
-
-  constructor() { }
+// To get data from user child component
+  constructor ( private route: ActivatedRoute ) { }
 
   ngOnInit() {
+
+    this.user = {
+      id:this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
+    
   }
 
 }
