0772f982c929ed203ba5cc3922209482acce2cb8
Mohsinpu424 <mi873345@gmail.com>
2023-07-07 16:49:03 +0500

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,25 +15,27 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 
 const appRoutes :Routes = [
   {path:'', component:HomeComponent},
-
+  
   {path:'users', component:UsersComponent, children: [
     {path:':id/:name', component:UsersComponent}
-            ]
-  },
+  ]
+},
 
-  
 
-  {path:'servers', component:ServersComponent , children :[
+
+{path:'servers', component:ServersComponent , children :[
   {path:':id', component:ServersComponent},
   
   {path: ':id/edit', component:EditServerComponent},
-             ]},
-  
+]},
+{path: 'not-found' , component: PageNotFoundComponent},
+{path: '**' , redirectTo:'/not-found'},
 ];
 
 
@@ -45,7 +47,8 @@ const appRoutes :Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
