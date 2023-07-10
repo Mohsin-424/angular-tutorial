d06213637a377fb1eaa57713d6791b56ed41895e
Mohsinpu424 <mi873345@gmail.com>
2023-07-10 15:33:43 +0500

    ccessing forms with @ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { NgForm } from "@angular/forms";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
+  @ViewChild('f') signuopForm!:NgForm;
+  
   suggestUserName() {
     const suggestedName = 'Superuser';
 
   }
-  onSubmit(form:NgForm){
-    console.log(form);
+  // onSubmit(form:NgForm){
+  //   console.log(form);
+  // }
+  onSubmit() {
+console.log(this.signuopForm);
   }
 }
 
