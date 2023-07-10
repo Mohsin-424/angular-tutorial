1250e92141056df31adad3cae06cc38be977f9ed
Mohsinpu424 <mi873345@gmail.com>
2023-07-10 18:06:56 +0500

    using NgMoDEL TWO WAY BINIDING IN ANGUALR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from "@angular/forms";
 export class AppComponent  {
   @ViewChild('f') signuopForm!:NgForm;
   defaultQuestion = 'Your Favourite Teacher';
+  answer = '';
   suggestUserName() {
     const suggestedName = 'Superuser';
 
