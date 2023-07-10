6788c497cca532d395e76a9286b65b0dff638101
Mohsinpu424 <mi873345@gmail.com>
2023-07-10 13:17:04 +0500

    Module Introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,12 @@
-import { Subscription } from 'rxjs';
-import { UserService } from './user/user.service';
-import { Component, OnInit,OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit,OnDestroy {
-  userActivated = false;
-// Store the subscription
-private activatedSub!: Subscription;
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    this.activatedSub = this.userService.activatedEmitter.subscribe((didActivate: boolean) => {
-      this.userActivated = didActivate;
-    });
-  }
-  ngOnDestroy(){
-    this.activatedSub.unsubscribe();
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'SuperUser';
   }
 }
