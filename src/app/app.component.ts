d9b0983d981abcbedf977b1efb5045797770a6d7
Mohsinpu424 <mi873345@gmail.com>
2023-07-11 12:25:00 +0500

    Reactive Forms Module Introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,11 @@
-import { NgForm } from '@angular/forms';
-import { Component,ViewChild} from '@angular/core';
-
+import { Component } from '@angular/core';
+import { FormGroup } from "@angular/forms";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('registrationForm', {static:false}) registrationForm!:NgForm;
-  user = {
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
-  };
-
-  onregister(registrationForm: NgForm) {
-    if (registrationForm.valid) {
-      // Perform registration logic here, such as sending data to an API
-      console.log('Registration data:', this.user);
-    }
-  }
-
-  // onSubmit(form: ElementRef){
-  //   console.log(form);
-    
-  // }
+genders = ['male', 'female'];
+signupForm: FormGroup
 }
