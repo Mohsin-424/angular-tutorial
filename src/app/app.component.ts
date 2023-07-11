fc54cf1918255ad2b9a1a9e8ec2d9061b21bef87
Mohsinpu424 <mi873345@gmail.com>
2023-07-11 08:24:36 +0500

    using the form data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,48 @@ import { NgForm } from "@angular/forms";
 export class AppComponent  {
   @ViewChild('f') signuopForm!:NgForm;
   defaultQuestion = 'Your Favourite Teacher';
+
   answer = '';
+
+  genders = ['male' , 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+submitted = false;
+  
   suggestUserName() {
     const suggestedName = 'Superuser';
-
+    //   this.signuopForm.setValue({
+    //     userData: {
+    //        username: suggestedName,
+    //        email: ''
+    //     },
+    //     secret: 'pet',
+    //     questionAnswer: '',
+    //     gender: 'male'
+    //   });
+    this.signuopForm.form.patchValue({
+      // patch value overwrties parts of a form
+      userData:{
+        // Set.value to set the whole frm
+        username:suggestedName
+    }});
   }
   // onSubmit(form:NgForm){
   //   console.log(form);
-  // }
+  
   onSubmit() {
 console.log(this.signuopForm);
+this.submitted = true;
+this.user.username = this.signuopForm.value.userData.username;
+this.user.email = this.signuopForm.value.userData.email;
+this.user.secretQuestion = this.signuopForm.value.secret;
+this.user.answer = this.signuopForm.value.questionAnswer;
+this.user.gender = this. signuopForm.value.gender;
   }
 }
 
