f2b4fe4034ca94b59a3f548e2bf5ea23f1270d87
Mohsinpu424 <mi873345@gmail.com>
2023-07-12 18:23:01 +0500

    lesson211

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,16 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm!: FormGroup;
+  forbiddenUsernames = ['Mohsin', 'Raza'];
 
   constructor() {}
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, 
+          
+          [Validators.required, this.forbiddenNames.bind(this)] ),
         email: new FormControl(null, [Validators.required, Validators.email]),
         gender: new FormControl('male'),
         hobbies: new FormArray([])
@@ -31,6 +35,14 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (this.signupForm.get('userData.hobbies') as FormArray).push(control);
+  }
+
+  forbiddenNames( control: FormControl): { [s:string]: boolean } {
+    // Logic to implement cvalidation of forbiedden user names
+    if( this. forbiddenUsernames.indexOf( control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+      return {'nameIsForbidden': true};
   }
 
   get hobbyControls() {
