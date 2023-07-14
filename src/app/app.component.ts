463b126d1e816739b1673d72a5d2dc34fddd6209
Mohsinpu424 <mi873345@gmail.com>
2023-07-14 15:30:51 +0500

    Lesson249

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // Appstatus for Aycnch pipe
+  Appstatus = new Promise((resolve, reject) =>{
+    setTimeout(() => {
+      resolve  ('stable')
+    }, 2000);
+  });
 
   servers = [
     {
